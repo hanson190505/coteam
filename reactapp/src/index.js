12ee91d30457fffb3fe03d540d81cd8506b914ea
hanson190505 <413506012@qq.com>
2020-08-18 12:25:14 +0800

    react learn

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -26,36 +26,131 @@ import * as serviceWorker from './serviceWorker';
 //   ReactDOM.render(element, document.getElementById('root'))
 // }
 // setInterval(tick, 1000)
-class Clock extends React.Component {
+// class Clock extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       date: new Date()
+//     }
+//   }
+//   componentDidMount() {
+//     this.timerID = setInterval(() => this.tick(), 1000)
+//   }
+//   componentWillUnmount() {
+//     clearInterval(this.timerID)
+//   }
+//   tick() {
+//     this.setState({
+//       date: new Date()
+//     })
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello world</h1>
+//         <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+//       </div>
+//     )
+//   }
+// }
+
+// ReactDOM.render(
+//   <Clock />,
+//   document.getElementById('root')
+// )
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault()
+    console.log('the link was clicked.');
+  }
+  return (
+    <a href="#" onClick={handleClick}>
+      Click me
+    </a>
+  )
+}
+class Toggle extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      date: new Date()
-    }
+    this.state = { isToggleOn: true }
+    this.handleClick = this.handleClick.bind(this)
   }
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000)
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID)
-  }
-  tick() {
-    this.setState({
-      date: new Date()
-    })
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn:!state.isToggleOn
+    }))
   }
   render() {
     return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn?'ON' : 'OFF'}
+      </button>
+    )
+  }
+}
+function UserGreeting(props) {
+  return <h1>Welcome back</h1>
+}
+function GuestGreeting(props) {
+  return <h1> please sign up </h1>
+}
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn
+  if (isLoggedIn) {
+    return <UserGreeting/>
+  }
+  return <GuestGreeting/>
+}
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+function LogoutButton(props) {
+  return ( <button onClick = {props.onClick}>
+    Logout </button>
+  )
+}
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleLoginClick = this.handleLoginClick.bind(this)
+    this.handleLogoutClick = this.handleLogoutClick.bind(this)
+    this.state = {isLoggedIn:false}
+  }
+  handleLoginClick() {
+    this.setState({isLoggedIn:true})
+  }
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false})
+  }
+  render() {
+    const isLoggedIn = this.state.isLoggedIn
+    let button
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick}/>
+    } else {
+      button = <LoginButton onClick = {this.handleLoginClick}/>
+    }
+    
+    
+    return (
       <div>
-        <h1>Hello world</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
       </div>
     )
   }
 }
-
 ReactDOM.render(
-  <Clock />,
+  <React.StrictMode>
+    <ActionLink />
+    <Toggle /> 
+    <LoginControl/>
+  </React.StrictMode>,
+  
   document.getElementById('root')
 )
 // If you want your app to work offline and load faster, you can change
