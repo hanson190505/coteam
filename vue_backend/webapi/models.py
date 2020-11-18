04f5d561d8ee3c6ca5beee42048e86bee3e1b462
hanson190505 <413506012@qq.com>
2020-11-18 09:48:00 +0800

    change product model fields length

--- vue_backend/webapi/models.py
+++ vue_backend/webapi/models.py
@@ -21,7 +21,7 @@ class Products(models.Model):
     sub_type = models.ForeignKey(
         ProductsType, null=True, blank=True, max_length=32, verbose_name='产品子类', on_delete=models.CASCADE)
     seo_title = models.CharField(max_length=128, default='china good gifts')
-    seo_desc = models.CharField(max_length=256, default='china good gifts')
+    seo_desc = models.CharField(max_length=1024, default='china good gifts')
     pub_date = models.DateField('添加日期', auto_now=datetime.now)
     pro_number = models.CharField('产品编号', unique=True, max_length=32)
     pro_name = models.CharField('产品名称', max_length=30, default='product')
@@ -32,18 +32,18 @@ class Products(models.Model):
     goods_num = models.IntegerField("库存数", default=0)
     is_new = models.IntegerField('是否新品', default=0)
     is_hot = models.IntegerField('是否热销', default=0)
-    capacities = models.CharField('容量', max_length=128, default='custom')
+    capacities = models.CharField('容量', max_length=256, default='custom')
     moq = models.IntegerField('起订量', default=100)
     imprint_methods = models.CharField('logo工艺', max_length=128, default='custom')
-    imprint_location = models.CharField('logo位置', max_length=64, default='custom')
+    imprint_location = models.CharField('logo位置', max_length=128, default='custom')
     imprint_size = models.CharField('logo尺寸', max_length=128, default='custom')
-    material = models.CharField('材质', max_length=64, default='custom')
+    material = models.CharField('材质', max_length=128, default='custom')
     # 商品折扣设置
     is_discount = models.IntegerField(default=0)
     pro_discount = models.DecimalField(max_digits=10, decimal_places=2, default=1)
-    pro_size = models.CharField('产品尺寸', max_length=64, default='custom made')
+    pro_size = models.CharField('产品尺寸', max_length=128, default='custom made')
     pro_model = models.CharField('产品型号', max_length=64, default='custom made')
-    pro_pack = models.CharField('产品包装', max_length=64, default='custom made')
+    pro_pack = models.CharField('产品包装', max_length=128, default='custom made')
     pro_color = models.CharField('产品颜色', max_length=1024, blank=True)
     pro_weight = models.DecimalField(verbose_name='单重(g)', max_digits=10, decimal_places=2, default=0)
     pro_desc = RichTextField()
@@ -59,8 +59,8 @@ class Products(models.Model):
 
 
 class HomeIndex(models.Model):
-    seo_title = models.CharField(max_length=64)
-    seo_desc = models.CharField(max_length=256)
+    seo_title = models.CharField(max_length=256)
+    seo_desc = models.CharField(max_length=1024)
     is_use = models.IntegerField(default=0)
     company_name = models.CharField(max_length=64, null=True, blank=True)
     company_addr = models.CharField(max_length=256, null=True, blank=True)
